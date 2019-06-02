222d2d493d9ce248e16d86d689bdb1f294697c60
bradythomas <bradythomas@SQ19CS-055>
2019-06-01 18:38:19 -0700

    messaging app

--- TeamSpace/Controllers/HomeController.cs
+++ TeamSpace/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TeamSpace.Models;
 
 namespace TeamSpace.Controllers
 {
@@ -35,8 +36,17 @@ namespace TeamSpace.Controllers
 
         public ActionResult Messages()
         {
-            ViewBag.Message = "Your Messages page.";
 
+            MessagesModel m = new MessagesModel
+            {
+                Message1 = "Did my daughter leave her uniform at your house?? I can't find it and its only a few hours away from the competition!!! I am freaking out!",
+                Message2 = "I have your daughters uniform. I washed it after she left it at my house when she had a sleepover with Michelle.I'll bring it to the competition.",
+                Message3 = "Hey, Just checking in.. Did you remember to bring the Gluten Free Snacks? Also, what is Gluten Free mean?",
+                Message4 = "Yes we are good! Also, check the FAQ page dude!",
+                Message5 = "Who is your daughter riding with?",
+                Message6 = "Who is your daughter riding with?"
+            };
+            ViewBag.Message = m;
             return View();
         }
 
